580b8051043cb88a679c7e45c5d485a049ae2f49
mitiay001 <2375036@gmail.com>
2023-06-06 19:28:38 +0300

    pochti vse ssiki vidosi logo

--- zapis.js
+++ zapis.js
@@ -9,11 +9,22 @@ document.getElementById('appointmentForm').addEventListener('submit', function(e
   
     // Ваш код обработки отправки формы
     // Например, можно отправить данные на сервер с помощью AJAX или другого метода
-    
+  
     // Очищаем значения полей формы
     document.getElementById('name').value = '';
     document.getElementById('email').value = '';
     document.getElementById('phone').value = '';
     document.getElementById('date').value = '';
+  
+    // Выводим уведомление
+    var notification = document.createElement('div');
+    notification.textContent = 'Спасибо, ' + name + ', за Ваш выбор. Ваша запись на ' + date + ' успешно подтверждена!';
+    notification.classList.add('notification');
+    document.body.appendChild(notification);
+  
+    // Убираем уведомление через 5 секунд
+    setTimeout(function() {
+      notification.remove();
+    }, 5000);
   });
   
